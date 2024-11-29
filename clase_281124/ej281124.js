6d55871fea51f2a318ecd8fa309c0c30e2ac3dec
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2024-11-28 21:49:57 -0600

    Se corrigen detalles en los documentos.

--- clase_281124/ej281124.js
+++ clase_281124/ej281124.js
@@ -1,5 +1,5 @@
-document.getElementById("Boton-tema").addEventListener("click", () => {
-    const body = document.body;
+document.getElementById("boton-tema").addEventListener("click",()=>{
+    const body = document.body
     body.classList.toggle("tema-claro");
     body.classList.toggle("tema-oscuro");
 })
