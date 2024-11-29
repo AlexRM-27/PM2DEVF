f2ab604e8d2f675e2d853e27676aadbf364d93c8
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2024-11-28 21:33:35 -0600

    Se añade formulario con sus propiedades y funcionalidad con el tema claro/oscuro.

---
+++ clase_281124/ej281124.js
@@ -0,0 +1,5 @@
+document.getElementById("Boton-tema").addEventListener("click", () => {
+    const body = document.body;
+    body.classList.toggle("tema-claro");
+    body.classList.toggle("tema-oscuro");
+})
